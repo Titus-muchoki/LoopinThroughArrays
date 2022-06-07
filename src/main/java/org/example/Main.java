44a0574aea371d5a4d36d27c42f7e76537924e06
kajela <titoyut56@gmail.com>
2022-06-07 01:43:17 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,20 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String Vehicle[] ={"BMW X6", "AUDI", "BENZ","VOLVO"};
+        for (String i : Vehicle) {
+            System.out.println(i);
+        }
+    int numbers [] = {1,3,4,5};
+        for (int z :numbers) {
+            System.out.println(z);
+        }
+    for (int i= 0; i < 8; i++) {
+        if (i ==2) {
+            break;
+        }
+        System.out.println(i);
+        }
     }
-}
+    }
